31627fe7d5897249477945cf7c36e27c359e7bf7
Raisa Dorzback <raisa.dorzback@gmail.com>
2024-04-10 12:39:44 -0400

    BUG FIX: Users can no longer add a spell to local storage if it already exists there (across sessions. Check mark / plus sign corresponds with whether or not that spell is already in local storage / the user's deck. JS converted to object-oriented format instead of functional

--- main.js
+++ main.js
@@ -1,153 +1,188 @@
-// event listener for search button
-document.querySelector('#search-btn').addEventListener('click', getFetch);
+class SpellManager {
+  constructor() {
+    this.plusSymbol = document.querySelector('#plus .fa-plus');
+    this.checkSymbol = document.querySelector('#plus .fa-check');
+    this.spellName = document.querySelector('#left-page h2');
+    this.components = document.querySelector('#components');
+    this.classes = document.querySelector('#classes');
+    this.subclasses = document.querySelector('#subclasses');
+    this.dmgType = document.querySelector('#dmg-type');
+    this.dmgUL = document.querySelector('#dmgUL');
+    this.desc = document.querySelector('#right-page p');
 
-// also search by hitting enter key and it will automagically make that a  click event
-document.querySelector('input').addEventListener('keypress', function(event) {
-  if (event.key == 'Enter') {
-      document.querySelector('#search-btn').click();
-  }
-});
-
-// event listener for adding spell to deck
-document.querySelector('#plus').addEventListener('click', addSpellToLocalStorage);
-
-// API fetch request
-function getFetch(){
-  let spell = document.querySelector('input').value.toLowerCase();
-  spell = spell.split(' ').join('-');
-  const url = `https://www.dnd5eapi.co/api/spells/${spell}`;
-
-  fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        // log data
-        console.log(data);
-
-        displayName(data);
-        displaySpellInfo(data);
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
-
-const displayName = data => {
-  return document.querySelector('#left-page h2').textContent = data.name;
-};
-const displaySpellInfo = data => {
-  displayDuration(data);
-  displayCastingTime(data);
-  displaySchool(data);
-  displayLevel(data);
-  displayRange(data);
-  displayClasses(data);
-  displayDesc(data);
-  displayComponents(data);
-  displayDmgType(data);
-  displayDmgAtCharLevel(data);
-};
-const displayDuration = data => {
-  return document.querySelector('#duration').textContent = data.duration;
-};
-const displayCastingTime = data => {
-  return document.querySelector('#c-time').textContent = data.casting_time;
-};
-const displaySchool = data => {
-  return document.querySelector('#school').textContent = data.school.name;
-};
-const displayLevel = data => {
-  return document.querySelector('#level').textContent = data.level;
-};
-const displayRange = data => {
-  return document.querySelector('#range').textContent = data.range;
-};
-const displayClasses = data => {
-  const classes = data.classes.map(e => e.name).join(', ')
-  document.querySelector('#classes').textContent = classes;
-
-  const subclasses = data.subclasses.map(e => e.name).join(', ')
-  document.querySelector('#subclasses').textContent = subclasses;
-};
-const displayDesc = data => {
-  return document.querySelector('#right-page p').textContent = data.desc[0];
-}; 
-const displayComponents = data => {
-  const components = data.components.join(', ')
-  document.querySelector('#components').textContent = components;
-};
-const displayDmgType = data => {
-  const dmgType = document.querySelector('#dmg-type');
-  if (data.damage) {
-    const damage = data.damage.damage_type.name;
-    dmgType.textContent = damage;
-  } else {dmgType.textContent = 'None';};
-};
-const displayDmgAtCharLevel = data => {
-  let dmgList = []
-  if(data.damage) {
-    if(data.damage.damage_at_character_level) {
-      for (const [key, value] of Object.entries(data.damage.damage_at_character_level)) {
-        dmgList.push(`${key}: ${value}`);
-      };
-    } else if(data.damage.damage_at_slot_level) {
-      for (const [key, value] of Object.entries(data.damage.damage_at_slot_level)) {
-        dmgList.push(`${key}: ${value}`);
-      };
-    }
-    // insert damage list into DOM
-    const dmgUL = document.querySelector('#dmgUL');
-    // clear pre-existing list
-    dmgUL.textContent = '';
-
-    // loop through damage at character level list
-    dmgList.forEach(e => {
-      // create list item element
-      let li = document.createElement('li');
-      // insert each level into li
-      li.appendChild(document.createTextNode(e));
-      // add item to the UL
-      dmgUL.appendChild(li);
+    // Event Listeners
+    // search button
+    document.querySelector('#search-btn').addEventListener('click', this.getFetch.bind(this));
+    
+    // hitting enter key will also search
+    document.querySelector('input').addEventListener('keypress', function(event) {
+      if (event.key == 'Enter') {
+        document.querySelector('#search-btn').click();
+      }
     });
 
+    document.querySelector('#plus').addEventListener('click', this.addSpellToLocalStorage.bind(this));
 
-  } else {
-    // remove damage at character level section from card
-    dmgUL.textContent = 'N/A';
-  };
+    // if local storage is empty
+    if(localStorage.length === 0) {
+      this.keyNum = 1; // start at 1
+    } else {
+      // start at where the last key left off
+      const maxKey = Math.max(...Object.keys(localStorage).map(Number));
+      this.keyNum = maxKey + 1;
+    }
+  }
 
-  console.log(dmgList)
-};
+  getFetch() {
+    let spell = document.querySelector('input').value.toLowerCase().split(' ').join('-');
+    const url = `https://www.dnd5eapi.co/api/spells/${spell}`;
 
-// key number so each spell has a unique number 
-// ***TO DO: instead, need to get the key number from local storage and start from the last number
-let keyNum = 1;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data.error || !data.name) {
+          this.displayErrorMessage();
+        } else {
+          this.displaySpellInfo(data);
+        };
+      })
+      .catch(err => {
+        console.log(`Error: ${err}`);
+      });
+  }
 
-// ***TO DO: If spell is already in local storage, have it start as a check on that spell. otherwise, have it start as a plus
+  displayErrorMessage() {
+    this.displayName('We couldn\'t find a spell by that name :( Try searching for another!');
+    this.displayDuration('N/A');
+    this.displayCastingTime('N/A');
+    this.displaySchool('N/A');
+    this.displayLevel('N/A');
+    this.displayRange('N/A');
+    this.subclasses.textContent = 'N/A';
+    this.classes.textContent = 'N/A';
+    this.components.textContent = 'N/A';
+    this.dmgType.textContent = 'N/A';
+    this.dmgUL.textContent ='N/A';
+    this.desc.textContent = 'There is no description available for this spell. Try to search for another spell!'
+  }
+  displaySpellInfo(data) {
+    // if spell is already in storage, show a check otherwise show a plus
+    this.spellIsInLocalStorage(data.name) ? this.plusToCheck() : this.checkToPlus();
+    this.displayName(data.name);
+    this.displayDuration(data.duration);
+    this.displayCastingTime(data.casting_time);
+    this.displaySchool(data.school.name);
+    this.displayLevel(data.level);
+    this.displayRange(data.range);
+    this.displayClasses(data.classes, data.subclasses);
+    this.displayDesc(data);
+    this.displayComponents(data);
+    this.displayDmgType(data);
+    this.displayDmgAtCharLevel(data);
+  }
+  displayName(name) {
+    this.spellName.textContent = name;
+  }
+  displayDuration(duration) {
+    document.querySelector('#duration').textContent = duration;
+  }
+  displayCastingTime(castingTime) {
+    document.querySelector('#c-time').textContent = castingTime;
+  }
+  displaySchool(school) {
+    document.querySelector('#school').textContent = school;
+  }
+  displayLevel(lvl) {
+    document.querySelector('#level').textContent = lvl;
+  }
+  displayRange(range) {
+    document.querySelector('#range').textContent = range;
+  }
+  displayClasses(cl, sc) {
+    const classes = cl.map(e => e.name).join(', ');
+    this.classes.textContent = classes;
 
-function addSpellToLocalStorage() {
-  // save spellname to a variable
-  const spellName = document.querySelector('#left-page h2').textContent
+    const subclasses = sc.map(e => e.name).join(', ');
+    this.subclasses.textContent = subclasses;
+  }
+  displayDesc(data) {
+    this.desc.textContent = data.desc[0];
+  }
+  displayComponents(data) {
+    const components = data.components.join(', ');
+    this.components.textContent = components;
+  }
+  displayDmgType(data) {
+    this.dmgType.textContent = data.damage ? data.damage.damage_type.name : 'None';
+  }
+  displayDmgAtCharLevel(data) {
+    let dmgList = []
+    if(data.damage) {
+      if(data.damage.damage_at_character_level) {
+        for (const [key, value] of Object.entries(data.damage.damage_at_character_level)) {
+          dmgList.push(`${key}: ${value}`);
+        };
+      } else if(data.damage.damage_at_slot_level) {
+        for (const [key, value] of Object.entries(data.damage.damage_at_slot_level)) {
+          dmgList.push(`${key}: ${value}`);
+        };
+      }
+
+      // clear pre-existing list
+      this.dmgUL.textContent = '';
   
-  // ***TO DO: if the spell is not already in local storage, then add the spell to it
-
-  // add spell name to local storage
-  localStorage.setItem(`spell${keyNum}`, spellName);
-  keyNum += 1; // increment key number
-
-  plusToCheck()
+      // loop through damage at character level list
+      dmgList.forEach(e => {
+        // create list item element
+        let li = document.createElement('li');
+        // insert each level into li
+        li.appendChild(document.createTextNode(e));
+        // add item to the UL
+        this.dmgUL.appendChild(li);
+      });
+  
+  
+    } else {
+      // remove damage at character level section from card
+      this.dmgUL.textContent = 'N/A';
+    };
+  }
+  addSpellToLocalStorage() {
+    const spellName =this.spellName.textContent;
+    
+    // if the spell is not already in local storage
+    if (!this.spellIsInLocalStorage(spellName)) {
+      this.checkToPlus();
+      // add it to local storage
+      localStorage.setItem(`${this.keyNum}`, spellName);
+      this.keyNum++;
+      this.plusToCheck(); // turn the plus to a check
+    } else {
+      console.log(`Spell '${spellName}' is already in local storage.`);
+      this.plusToCheck();
+    };
+  }
+  spellIsInLocalStorage(spellName) {
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      // Convert key to number before comparison
+      const numKey = parseInt(key);
+      if (!isNaN(numKey) && localStorage.getItem(key) === spellName) {
+        return true;
+      }
+    }
+    return false;
+  }
+  plusToCheck() {
+    this.plusSymbol.classList.add('hidden');
+    this.checkSymbol.classList.remove('hidden');
+  }
+  checkToPlus() {
+    this.plusSymbol.classList.remove('hidden');
+    this.checkSymbol.classList.add('hidden');
+  }
 };
 
-// variables to store plus and check symbols
-const plusSymbol = document.querySelector('#plus .fa-plus');
-const checkSymbol = document.querySelector('#plus .fa-check');
-
-// When someone adds a spell to their deck or has it added, change the plus sign to a check so they know they already have it
-function plusToCheck() {
-  plusSymbol.classList.add('hidden');
-  checkSymbol.classList.remove('hidden');
-};
-
-function checkToPlus() {
-  plusSymbol.classList.remove('hidden');
-  checkSymbol.classList.add('hidden');
-};
+// instantiate spellManager
+const spellManager = new SpellManager();
